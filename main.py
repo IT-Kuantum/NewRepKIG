9216a90508191ee1624231b714e616e87c6a262e
IT-Kuantum <IT-Kuantum@yandex.ru>
2020-10-27 16:55:55 +1000

    0

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-import os
 
 W = 800
 H = 600
@@ -7,22 +6,22 @@ BLACK = (0, 0, 0)
 WHITE = (225, 225, 225)
 YELLOW = (225, 225, 0)
 RED = (255, 0, 0)
-BLUE = (0, 0, 255)
+BLUE = (0, 0, 50)
 
 pygame.init()
 pygame.display.set_caption('Текст')
 screen = pygame.display.set_mode((W, H))
 
-pygame.draw.rect(screen, (RED), (370, 180, 50, 50))
 
 font = pygame.font.SysFont('Arial', 50, True, False)
 font1 = pygame.font.SysFont('Arial', 26, True, False)
 
-path = os.path.dirname(os.path.abspath(__file__))
-image = pygame.image.load
-
+screen.fill(BLUE)
+pygame.draw.rect(screen, (RED), (370, 180, 50, 50))
 screen.blit(font.render('Всем привет', True, WHITE),(250, 250))
 screen.blit(font1.render('задание на урок', True, YELLOW), (300, 310))
+
+
 pygame.display.update()
 run = True
 while run:
